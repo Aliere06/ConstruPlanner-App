f03c200323ab876f5e49903e2b05317c047655a5
Aliere <allieig666@gmail.com>
2024-05-07 19:33:31 -0600

    Información de estructuras panel B

---
+++ piso concreto.gd
@@ -0,0 +1,14 @@
+extends Panel
+var metraje
+var costo
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	InputEventMouseButton
+	var btnConcreto = gui_input
+	pass
