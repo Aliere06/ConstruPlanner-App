5f4afa69fd6c19bdb9a3d3b3fc24a97b5d2ae8dc
Aliere <allieig666@gmail.com>
2024-05-11 23:03:50 -0600

    Organización

--- PanelC.gd
+++ PanelC.gd
@@ -3,14 +3,12 @@ var info_Estructura
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	info_Estructura = load("res://Info_Estructura.tscn")
-	pass # Replace with function body.
-
+	info_Estructura = load("res://Paneles/Panel C/Info_Estructura.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_button_btn_concreto():
+func _on_button_btn_estructura(boton):
+	print(boton.name)
 	add_child(info_Estructura.instantiate())
-	pass # Replace with function body.
--- piso concreto.gd
+++ piso concreto.gd
@@ -1,10 +1,16 @@
 extends Panel
 
+signal btn_Concreto
+
+func _gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print(get_parent().name + ": I've been clicked D:")
+			emit_signal("btn_Concreto")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
